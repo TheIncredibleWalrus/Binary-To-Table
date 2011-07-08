7903aea1213ae372f442bac31624fc3b5f635ea6
Ethan Parker <ethan.calculatorz@gmail.com>
2011-07-08 16:24:59 -0400

    creation of main source code file... it does nothing yet, though.

---
+++ BinaryToTable.java
@@ -0,0 +1,14 @@
+   public class BinaryToTable {
+      public static void main(String[] arguments)
+      {
+         if(arguments.length != 2)
+         {
+            System.out.println("Error: Too many or too few program arguments.");
+            System.out.print("Usage: java BinaryToTable binaryfile tablefile (use quotes for filenames with spaces)");
+         }
+         else
+      	{
+      		
+      	}
+      }
+   }
